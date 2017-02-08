756c8c178dba900f513caabbf4a93d02e51ec58e
Troels Brødsgaard <troels@uptime.dk>
2017-02-08 20:57:25 +0100

    Elaborate cEMI tests

--- test/knxnet_ip/cemi_test.exs
+++ test/knxnet_ip/cemi_test.exs
@@ -18,7 +18,7 @@ defmodule KNXnetIP.CEMITest do
         0x01::4, 0x01::4, 0x01::8, # Source (SAH, SAL)
         0x00::4, 0x00::4, 0x03::8, # Destination (DAH, DAL)
         0x00::1, 0x00::3, 0x03::4, # AT, NPCI, octet count/NPDU length
-        0x00::6, 0x02::4, 0x00::6, # TPCI, application control field, APCI/data <- these last 6 bits can be used to contain the data if the data is equal to or less than 6 bits - see p 13. of Application Layer specification
+        0x00::6, 0x02::4, 0x00::6, # TPCI, APCI (application control field), APCI/data <- these last 6 bits can be used to contain the data if the data is equal to or less than 6 bits - see p 13. of Application Layer specification
         0x1917::16, # data
       >>
 
@@ -26,7 +26,7 @@ defmodule KNXnetIP.CEMITest do
       assert encoded == CEMI.encode(decoded)
     end
 
-    test "decode/encode A_GroupValue_Read" do
+    test "decode/encode A_GroupValue_Read with 1 byte octet count" do
       decoded = %CEMI.LDataInd{
         source: "1.0.3",
         destination: "0/0/3",
@@ -35,12 +35,12 @@ defmodule KNXnetIP.CEMITest do
       }
 
       encoded = <<
-        0x29, 0x00,
-        0xBC, 0xE0,
-        16, 3,
-        0, 3,
-        1, 0,
-        0
+        0x29, 0x00, # Message code, additional info length
+        0xBC, 0xE0, # ctrl1, ctrl2,
+        0x01::4, 0x00::4, 0x03::8, # Source (SAH, SAL)
+        0x00::4, 0x00::4, 0x03::8, # Destination (DAH, DAL)
+        0x00::1, 0x00::3, 0x01::4, # AT, NPCI, octet count/NPDU length
+        0x00::6, 0x00::4, 0x00::6, # TPCI, application control field, APCI/data
       >>
 
       assert decoded == CEMI.decode(encoded)
